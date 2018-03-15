4b5fcc645996fbcd0f0dd5d78bbcc5f30cd4e198
AlexandrProkopenko <prokopenko89@gmail.com>
2018-03-15 21:00:13 +0200

    First

---
+++ src/main/java/util/HibernateUtil.java
@@ -0,0 +1,22 @@
+package util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
+public class HibernateUtil {
+
+    private static final SessionFactory factory = build();
+    private static StandardServiceRegistry registry;
+
+    private static SessionFactory build() {
+        registry = new StandardServiceRegistryBuilder().
+                configure("hibernate/hibernate.cfg.xml").
+                build();
+        return new MetadataSources(registry).
+                buildMetadata().
+                buildSessionFactory();
+    }
+}
